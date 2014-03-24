e63add91c3347b7c95e569a4bb081ebe81555d2a
Michael Smith <theguitarizt@gmail.com>
2014-03-24 07:16:16 -0400

    adding code from book

---
+++ Scala/ProgrammingInScala/Chapter3/step-11.scala
@@ -0,0 +1,20 @@
+def printArgs(args: Array[String]): Unit = {
+    var i = 0
+    while (i < args.length) {
+        println(args(i))
+        i += 1
+    }
+}
+
+def printArgs(args: Array[String]): Unit = {
+    for(arg <- args)
+        println(arg)
+}
+
+def printArgs(args: Array[String]): Unit = {
+    args.foreach(println)
+}
+
+def formatArgs(args: Array[String]) = args.mkString("\n")
+println(formatArgs(args))
+
