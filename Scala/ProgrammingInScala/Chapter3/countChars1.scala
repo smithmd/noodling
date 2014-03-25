f2f5a01acf192ea398a08d34d22bf53fb03832c3
Michael Smith <theguitarizt@gmail.com>
2014-03-25 07:37:21 -0400

    countChars changed

--- Scala/ProgrammingInScala/Chapter3/countChars1.scala
+++ Scala/ProgrammingInScala/Chapter3/countChars1.scala
@@ -5,8 +5,17 @@ def widthOfLength(s: String) = s.length.toString.length
 if (args.length > 0) {
 
   val lines = Source.fromFile(args(0)).getLines().toList
+  
+  val longestLine = lines.reduceLeft(
+    (a, b) => if (a.length > b.length) a else b    
+  )
+
+  val maxWidth = widthOfLength(maxWidth)
+  
   for (line <- lines)
-    println(line.length +" "+ line)
+    val numSpaces = maxWidth - widthOfLength(line)
+    val padding = " " * numSpaces
+    println(padding + line.length +" "+ line)
 }
 else
   Console.err.println("Please enter filename")
