18eecc9dd6802d76561b37141da48c3baae5ce27
Michael Smith <theguitarizt@gmail.com>
2014-04-22 07:41:36 -0400

    added singleton object to checksum file

--- Scala/ProgrammingInScala/Chapter4/CheckSumAccumulator.scala
+++ Scala/ProgrammingInScala/Chapter4/CheckSumAccumulator.scala
@@ -1,3 +1,21 @@
+import scala.collection.mutable.Map;
+
+object ChecksumAccumulator {
+    private val cache = Map[String, Int]()
+    
+    def calculate(s: String): Int = 
+        if (cache.contains(s)) {
+            cache(s)
+        } else {
+            val acc = new ChecksumAccumulator
+            for(c <- s)
+                acc.add(c.toByte)
+            val cs = acc.checksum()
+            cache += (s -> cs)
+            cs
+        }
+}
+
 class ChecksumAccumulator {
   private var sum = 0
   
