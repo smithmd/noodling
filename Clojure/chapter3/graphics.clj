cde47c16823baffdd0a009215d5a5f224f5d08ee
Michael Smith <smithmd@interlochen.org>
2014-10-13 16:58:25 -0400

    small tweaks

--- Clojure/chapter3/graphics.clj
+++ Clojure/chapter3/graphics.clj
@@ -1,20 +1,17 @@
-(defn xors [max-x max-y]
-  (for [x (range max-x)
-        y (range max-y)]
-    [x y (rem (bit-xor x y) 256)]))
-
 (defn f-values [f xs ys]
   (for [x (range xs)
         y (range ys)]
     [x y (rem (f x y) 256)]))
 
 (def frame (java.awt.Frame.))
-
 (.setVisible frame true)
 
 (def gfx (.getGraphics frame))
 
+
 (defn clear [g] (.clearRect g 0 0 200 200))
+
+
 
 (defn draw-values [f xs ys]
   (clear gfx)
@@ -24,5 +21,6 @@
     (.fillRect gfx x y 1 1)))
 
 (draw-values bit-and 256 256)
-
+(draw-values + 256 256)
+(draw-values * 256 256)
 
