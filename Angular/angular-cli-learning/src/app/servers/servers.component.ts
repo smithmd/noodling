7e034483c4156791eb63bbd40391de4fd2829d55
Michael Smith <smithmd@interlochen.org>
2017-05-05 15:24:14 -0400

    data binding

--- Angular/angular-cli-learning/src/app/servers/servers.component.ts
+++ Angular/angular-cli-learning/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName: string;
 
   constructor() {
     setTimeout(() => {
@@ -20,5 +21,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
