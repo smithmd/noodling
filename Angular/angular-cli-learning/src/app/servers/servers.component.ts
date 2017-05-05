810f4459a44a3a70e1ef4d7f18f752eae184aea2
Michael Smith <smithmd@interlochen.org>
2017-05-05 15:33:40 -0400

    before directives

--- Angular/angular-cli-learning/src/app/servers/servers.component.ts
+++ Angular/angular-cli-learning/src/app/servers/servers.component.ts
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = this.serverName + ' was created';
   }
 
   onUpdateServerName(event: Event) {
